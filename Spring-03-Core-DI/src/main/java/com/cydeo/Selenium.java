ccd62dd3ac16c01508279d4a59f66139bc65694b
Cameron Cisneros <cisneroscameron16@gmail.com>
2023-09-02 01:05:28 -0500

    injection

---
+++ Spring-03-Core-DI/src/main/java/com/cydeo/Selenium.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+public class Selenium {
+
+    OfficeHours officeHours;
+    public void getTeachingHours(){
+        System.out.println("weekly teacher hours"+(20+ officeHours.getHours()));
+    }
+
+
+
+}
